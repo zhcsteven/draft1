2e62978d57fc29ff646dc9a7dde66a778b45cfb4
zhcsteven <zhcsteven@github.com>
2020-10-06 20:28:34 -0500

    draft
    1

---
+++ mfscrm/crm/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path, re_path
+
+app_name = 'crm'
+urlpatterns = [
+    path('', views.home, name='home'),
+    re_path(r'^home/$', views.home, name='home'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('service_list', views.service_list, name='service_list'),
+    path('service/create/', views.service_new, name='service_new'),
+    path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
+    path('customer/<int:pk>/summary/', views.summary, name='summary'),
+]
+
+
